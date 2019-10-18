f8165773231ef05e96188d6a246ef82f072eea2c
Vasilios Magriplis <etameson@gmail.com>
2019-10-18 23:42:25 +1100

    Update eslint

--- src/index.js
+++ src/index.js
@@ -7,32 +7,21 @@ import HomeRoute from './home-route';
 import OtherRoute from './other-route';
 import './styles.scss';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props) 
-  }
-
-  render() {
-    return (
-      <div className="main">
-        <HashRouter>
-          <div>
-            <nav>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/otherroute">Other Route</Link></li>
-              </ul>
-            </nav>
-            <Switch>
-              <Route exact path="/" component={HomeRoute} />
-              <Route path="/otherroute" component={OtherRoute} />
-            </Switch>
-          </div>
-        </HashRouter>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <div className="main">
+    <HashRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/otherroute">Other Route</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={HomeRoute} />
+        <Route path="/otherroute" component={OtherRoute} />
+      </Switch>
+    </HashRouter>
+  </div>;
 
 render(
   <App />,

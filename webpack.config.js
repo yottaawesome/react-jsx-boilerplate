aef3e1c3d06c3a65d4c42be4db93ddc1c9a771f6
Vasilios Magriplis <etameson@gmail.com>
2020-08-09 14:57:53 +1000

    Update deps

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = (env, argv) => {
               options: {
                 modules: {
                   localIdentName: "[folder]__[local]__[hash:base64:5]",
-		  exportLocalsConvention: "camelCase"
+		   exportLocalsConvention: "camelCase"
                 }
               }
             },
